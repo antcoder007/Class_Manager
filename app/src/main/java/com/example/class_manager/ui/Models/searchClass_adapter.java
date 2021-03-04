aab6396de612227540153b43cb7ba71c5ce84f6f
Premanshu Yadav <contact@premanshu.com>
2021-03-04 17:26:26 -0500

    New adapter class to handle data semantics and layout for cardview rows and update them dynamically as per the data fetched.

---
+++ app/src/main/java/com/example/class_manager/ui/Models/searchClass_adapter.java
@@ -0,0 +1,53 @@
+package com.example.class_manager.ui.Models;
+
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.class_manager.R;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+
+public class searchClass_adapter extends FirebaseRecyclerAdapter<SearchClass, searchClass_adapter.viewHolder> {
+
+    public searchClass_adapter(@NonNull FirebaseRecyclerOptions<SearchClass> options) {
+        super(options);
+    }
+
+    @Override
+    protected void onBindViewHolder(@NonNull viewHolder holder, int position, @NonNull SearchClass course_model) {
+        holder.description.setText(course_model.getDescription());
+        holder.name.setText(course_model.getName());
+        holder.professor.setText(course_model.getProfessor());
+        //holder.img... Use this to set image for each task
+    }
+
+    @NonNull
+    @Override
+    public viewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.single_class_row,parent,false);
+        return new viewHolder(view);
+    }
+
+    class viewHolder extends RecyclerView.ViewHolder {
+
+        ImageView img;
+        TextView name, description, professor;
+
+        public viewHolder(@NonNull View itemView) {
+            super(itemView);
+            img = (ImageView)itemView.findViewById(R.id.img1);
+            name = (TextView)itemView.findViewById(R.id.course_name);
+            description = (TextView)itemView.findViewById(R.id.course_description);
+            professor = (TextView)itemView.findViewById(R.id.professor_name);
+        }
+    }
+
+}
