c525deb56b427597b6ef40a271bfca622b3b628e
Premanshu Yadav <contact@premanshu.com>
2021-03-04 17:24:59 -0500

    Added link to database and connection to adapter to display courses dynamically.

--- app/src/main/java/com/example/class_manager/ui/Models/search_class_activity.java
+++ app/src/main/java/com/example/class_manager/ui/Models/search_class_activity.java
@@ -6,14 +6,22 @@ import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.class_manager.MainActivity;
 import com.example.class_manager.R;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class search_class_activity extends AppCompatActivity {
 
     FloatingActionButton addClass;
+
+    RecyclerView recView;
+
+    searchClass_adapter course_adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +37,29 @@ public class search_class_activity extends AppCompatActivity {
             }
         });
 
+        recView = findViewById(R.id.class_recView);
+        recView.setLayoutManager(new LinearLayoutManager(this));
+
+        FirebaseRecyclerOptions<SearchClass> options =
+                new FirebaseRecyclerOptions.Builder<SearchClass>()
+                        .setQuery(FirebaseDatabase.getInstance().getReference().child("SearchClass"), SearchClass.class)
+                        .build();
+
+        course_adapter = new searchClass_adapter(options);
+        recView.setAdapter(course_adapter);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        course_adapter.startListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        course_adapter.stopListening();
     }
 
 }
